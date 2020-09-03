a151b63c6d883a3a8d827ba381de1db785a6b479
surferjreb <jamesbrownre1@gmail.com>
2020-09-02 22:37:15 -0400

    added player view

---
+++ app/src/main/java/com/softcoreinc/you_tube_app/YouTubeActivity.kt
@@ -0,0 +1,45 @@
+package com.softcoreinc.you_tube_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+
+class YouTubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_you_tube)
+//        val layout = findViewById<ConstraintLayout>(R.id.activity_you_tube)
+        val layout = layoutInflater.inflate(R.layout.activity_you_tube, null) as ConstraintLayout
+        setContentView(layout)
+
+//        val button1 = Button(this)
+//        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
+//        button1.text = "button_added"
+//        layout.addView(button1)
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("Not yet implemented")
+    }
+}
